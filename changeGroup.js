c9b92f1323d0b75a7dafaee95ccedcef0d9f49f5
firas <firastayeb1@gmail.com>
2019-06-30 14:38:55 +0200

    add user to group

---
+++ changeGroup.js
@@ -0,0 +1,20 @@
+
+var AWS = require('aws-sdk');
+var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({apiVersion: '2016-04-18'});
+
+export async function main(event, context) {
+    var params = {
+      GroupName: 'admin',
+      UserPoolId: event.userPoolId,
+      Username: event.requestContext.identity.cognitoIdentityId
+    };
+  
+    cognitoidentityserviceprovider.adminAddUserToGroup(params, function(err, data) {
+      if (err) console.log("Error");
+      else     console.log("Success");
+    });
+  
+    console.log("Executed.");
+  
+    context.succeed(event);
+}
